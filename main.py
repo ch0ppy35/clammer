b84c39f3b6bd49ef2f33465ae760236445be601f
Mike Miller <chop3593@gmail.com>
2024-02-13 20:58:24 -0700

    Clean

--- main.py
+++ main.py
@@ -30,18 +30,19 @@ def update_databases() -> None:
 
 def keep_updating() -> None:
     while True:
-        l.info({"message": "Performing update"})
+        l.info({"message": "Running update"})
         update_databases()
         time.sleep(60 * 60 * UPDATE_FREQUENCY)
 
 
 def start_updating_thread() -> None:
-    l.info({"message": "Performing initial update"})
+    l.info({"message": "Running initial update"})
     t = threading.Thread(target=keep_updating)
     t.start()
 
 
 if __name__ == "__main__":
+    l.info({"message": f"CLAM_DIR is set to {CLAM_DIR}"})
     try:
         start_updating_thread()
         l.info({"message": "Starting web server"})
@@ -50,9 +51,14 @@ if __name__ == "__main__":
             with socketserver.TCPServer(
                 ("0.0.0.0", WEB_PORT), http.server.SimpleHTTPRequestHandler
             ) as httpd:
-                l.info({"message": f"Now serving at port TCP {WEB_PORT}", "web": f"http://0.0.0.0:{WEB_PORT}/" })
+                l.info(
+                    {
+                        "message": f"Now serving on port {WEB_PORT}",
+                        "web": f"http://0.0.0.0:{WEB_PORT}/",
+                    }
+                )
                 httpd.serve_forever()
         except Exception as e:
             l.error({"message": f"Failed bringing up the web server - {e}"})
     except KeyboardInterrupt:
-        l.info({"message": "Program interrupted. Exiting gracefully."})
+        l.info({"message": "Exiting"})
